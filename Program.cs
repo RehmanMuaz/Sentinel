ab31051f4e1c144cf23400d9d3624564cbbf9ccf
RehmanMuaz <muaz_r5@Hotmail.com>
2025-12-03 00:56:27 -0500

    Add DBContextFactory

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OpenIddict.EntityFrameworkCore;
+using Sentinel.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SentinelDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("Default"));
+    options.UseOpenIddict();
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Sentinel.Infrastructure/SentinelDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OpenIddict.EntityFrameworkCore;
+
+namespace Sentinel.Infrastructure;
+
+public class SentinelDbContext : DbContext
+{
+    public SentinelDbContext(DbContextOptions<SentinelDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.UseOpenIddict();
+
+        // TODO: Add entity configurations here (Users, Tenants, etc.)
+    }
+}
